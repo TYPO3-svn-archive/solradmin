3b8ce3eb622ea6e0d1bdd7b273bf488d452826b9
ycerdan <ycerdan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-10 09:49:31 +0000

    First commit, 1.1.2 TER version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solradmin/trunk@60544 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "solradmin".
+#
+# Auto generated 02-04-2012 14:03
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Solr Administration',
+	'description' => 'With this module you can directly access your solr module, search records with an interface, delete some specifics records and do other operations (with multi core support)',
+	'category' => 'module',
+	'shy' => 0,
+	'version' => '1.1.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'CERDAN Yohann',
+	'author_email' => 'cerdanyohann@yahoo.fr',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"dab4";s:12:"ext_icon.gif";s:4:"11e4";s:14:"ext_tables.php";s:4:"0075";s:41:"classes/class.tx_solradmin_connection.php";s:4:"b7a4";s:14:"doc/manual.sxw";s:4:"d38a";s:13:"mod1/conf.php";s:4:"6ae3";s:14:"mod1/index.php";s:4:"f9aa";s:18:"mod1/locallang.xml";s:4:"ed07";s:22:"mod1/locallang_mod.xml";s:4:"2f4b";s:19:"mod1/moduleicon.gif";s:4:"11e4";s:40:"modfunc1/class.tx_solradmin_modfunc1.php";s:4:"a2af";s:22:"modfunc1/locallang.xml";s:4:"7f31";}',
+);
+
+?>
